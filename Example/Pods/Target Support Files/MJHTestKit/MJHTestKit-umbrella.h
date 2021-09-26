12c78a242000ca49031e07d80f4454c4fb555854
coco_os <905004880@qq.com>
2021-09-26 10:20:21 +0800

    add ext

--- Example/Pods/Target Support Files/MJHTestKit/MJHTestKit-umbrella.h
+++ Example/Pods/Target Support Files/MJHTestKit/MJHTestKit-umbrella.h
@@ -10,7 +10,35 @@
 #endif
 #endif
 
+#import "GlobalHeader.h"
 #import "MMTestClass.h"
+#import "NSArray+title.h"
+#import "NSDate + Category.h"
+#import "NSDate+Addtion.h"
+#import "NSDictionary+GYEUtility.h"
+#import "NSError + Addtation.h"
+#import "NSString + filte.h"
+#import "NSString+Emoji.h"
+#import "UIAlertController+Blocks.h"
+#import "UIAlertController+GYE.h"
+#import "UIButton+EventInterval.h"
+#import "UIButton+Position.h"
+#import "UIButton+touch.h"
+#import "UIImage + Color.h"
+#import "UIImage + GetMiddleImage.h"
+#import "UIImage+GYERoundImage.h"
+#import "UIImage+Tool.h"
+#import "UIImageView+Addition.h"
+#import "UIImageView+DoctorAvatar.h"
+#import "UILabel + AddUnit.h"
+#import "UILabel+GYELineSpace.h"
+#import "UITextField+Additions.h"
+#import "UITextView+PlaceHolder.h"
+#import "UIView + Tools.h"
+#import "UIView+Addtion.h"
+#import "UIView+ComMethod.h"
+#import "UIViewController+GYEAlert.h"
+#import "UIViewController+PresentViewController.h"
 
 FOUNDATION_EXPORT double MJHTestKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char MJHTestKitVersionString[];
